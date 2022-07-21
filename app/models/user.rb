a908e7231ab34e7fbde3dd135244198759f36f4a
Eric Dokter <edokter11@gmail.com>
2022-07-21 15:07:08 -0600

    test user spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,16 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
-  validates :password, presence: true
+  validates :password, presence: true, length: { minimum: 5 }
   validates :password_confirmation, presence: true
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.strip.downcase)
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
   
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,15 +5,19 @@ RSpec.describe User, type: :model do
     it 'validates new user' do
       @user = User.new(first_name: "Brian", last_name: "Regal", email: "example@yahoo.com", password: "12345", password_confirmation: "12345")
       @user.save
-
       expect(@user.save).to be true
     end
 
-    it 'name cannot be blank' do
-      @user = User.new(first_name: nil, last_name: "Regal", email: "example@yahoo.com", password: "12345", password_confirmation: "12345")
+    it 'first name cannot be blank' do
+      @user = User.new(first_name: nil, last_name: nil, email: "example@yahoo.com", password: "12345", password_confirmation: "12345")
       @user.save
-      p @user.errors.full_messages
-      expect(@user.errors.full_messages).to include("Name can't be blank")
+      expect(@user.errors.full_messages).to include("First name can't be blank")
+    end
+
+    it 'last name cannot be blank' do
+      @user = User.new(first_name: "Brain", last_name: nil, email: "example@yahoo.com", password: "12345", password_confirmation: "12345")
+      @user.save
+      expect(@user.errors.full_messages).to include("Last name can't be blank")
     end
 
     it 'passwords must match' do
@@ -22,6 +26,20 @@ RSpec.describe User, type: :model do
 
       expect(@user.save).to be false
       expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
+    it 'password cannot be blank' do
+      @user = User.new(first_name: "Brian", last_name: "Regal", email: "example@yahoo.com", password: nil, password_confirmation: nil)
+      @user.save
+      expect(@user.save).to be false
+      expect(@user.errors.full_messages).to include("Password can't be blank")
+    end
+
+    it 'password must be longer than 5 characters' do
+      @user = User.new(first_name: "Brian", last_name: "Regal", email: "example@yahoo.com", password: nil, password_confirmation: nil)
+      @user.save
+      
+      expect(@user.errors.full_messages).to include("Password is too short (minimum is 5 characters)")
     end
 
     it 'should reject user if no password is given' do
@@ -36,7 +54,6 @@ RSpec.describe User, type: :model do
       @user = User.new(first_name: "Brian", last_name: "Regal", email: nil, password: "12345", password_confirmation: "12345")
       @user.save
 
-      p @user.errors.full_messages
       expect(@user.errors.full_messages).to include("Email can't be blank")
     end
 
@@ -49,5 +66,48 @@ RSpec.describe User, type: :model do
       expect(@user2.errors.full_messages).to include("Email has already been taken")
 
     end
+  end
+
+  describe '.authenticate_with_credentials' do
+    before(:each) do 
+      @user = User.new(first_name: "Brian", last_name: "Regal", email: "example@yahoo.com", password: "12345", password_confirmation: "12345")
+      @user.save
+    end
+
+    it 'should not authenticate if password is wrong' do
+      params = { email: "example@yahoo.com", password: "67890" }
+      @user = User.authenticate_with_credentials(params[:email], params[:password])
+
+      expect(@user).to be_nil
+    end
+
+    it 'should not authenticate if email does not exist' do
+      params = { email: "wrong@yahoo.com", password: "12345" }
+      @user = User.authenticate_with_credentials(params[:email], params[:password])
+
+      expect(@user).to be_nil
+    end
+
+    it 'should authenticate if email and password match' do
+      params = { email: "example@yahoo.com", password: "12345" }
+      @user = User.authenticate_with_credentials(params[:email], params[:password])
+
+      expect(@user).to be_present
+    end
+
+    it 'should authenticate if email has whitespace' do
+      params = { email: " example@yahoo.com ", password: "12345" }
+      @user = User.authenticate_with_credentials(params[:email], params[:password])
+
+      expect(@user).to be_present
+    end
+
+    it 'should authenticate if email has wrong cases' do
+      params = { email: "EXAMPLE@yahoo.com", password: "12345" }
+      @user = User.authenticate_with_credentials(params[:email], params[:password])
+
+      expect(@user).to be_present
+    end
+
   end
 end
